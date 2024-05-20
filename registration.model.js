fe3b44455137c2185dc75b68855c95c0fe4981fb
Your Name <you@example.com>
2024-05-20 11:45:30 +0300

    .

--- registration.model.js
+++ registration.model.js
@@ -1,18 +1,25 @@
 import { Schema, model, connect } from 'mongoose';
 
+const addressSchema = new Schema({
+    city: { type: String , required: true },
+    neighbored: { type: String , required: true },
+    street: { type: String , required: true },
+    houseNumber: { type: String , required: true },
+    
+})
 
 const participantsSchema = new Schema({
-    name: String,
-    phone: String
+    name: { type: String, required: true },
+    phone: { type: String, required: true },
 });
 
 const registrationSchema = new Schema({
-    name: String,
-    phone: String,
-    address: String,
-    // participants: [participantsSchema],
-    participants: {},
-    vacation: String
+    beenThisYear: { type: Boolean, required: true },
+    periodSelected: { type: String, enum: ['first', 'second'] },
+    name: { type: String, required: true },
+    phone: { type: String, required: true },
+    address: { type: addressSchema, required: true },
+    participants: [participantsSchema],
 });
 
 export default model('Registration', registrationSchema);
