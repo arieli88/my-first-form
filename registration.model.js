0b7cf964717592f73a49ccd79e7636355c6b2209
Your Name <you@example.com>
2024-05-20 14:42:19 +0300

    on going

--- registration.model.js
+++ registration.model.js
@@ -18,11 +18,15 @@ const participantsSchema = new Schema({
 });
 
 const registrationSchema = new Schema({
-    beenThisYear: { type: Boolean, required: true },
+    // 1
+    beenThisYear: { type: Boolean, required: true }, 
+    // 2
     periodSelected: { type: String, enum: ['first', 'second'] },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    phone: { type: String, required: true },
+    cellPhone: { type: String, required: true },
+    homePhone: { type: String, required: false },
+    // 3
     address: { type: addressSchema, required: true },
     participantsNumber: { type: {
         adults: { type: Number, required: true },
@@ -30,11 +34,29 @@ const registrationSchema = new Schema({
         // computed field, should be equal to the sum of adults and children
         // sum: Number,
     }, required: true },
+    // 4
     participants: [participantsSchema],
+    // 5
+    forceKind: { type: String, required: true},
+    // 6 פרטי החלל
+    deceased: { type: {
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
+    }, required: true },
+    // 7
+    limitMedicals: {type: String},
+    // 8
+    specialRequest: {type: String},
+    // 9
+    shuttle: { type: String, enum: ['Tel-Aviv', 'Haifa', 'Jerusalem', 'Ber-sheva'], default: null  },
+    
+
 });
 
 registrationSchema.virtual('participantsNumber.sum').get(function() {
-    return this.participantsNumber.adults + this.participantsNumber.children;
+    console.log("סכום המשתתפים מבוגרים וילדים")
+    return this.participantsNumber.adults + this.participantsNumber.children
 });
 
-export default model('Registration', registrationSchema);
+export default model('Registration', registrationSchema)
+
