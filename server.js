d00edf0c723297cf69f0b7f774a1f08452bb929c
Yoel Faivelzon <yoelf@balink.net>
2024-05-21 14:04:28 +0300

    feat: Add environment variable support for development server & more

--- server.js
+++ server.js
@@ -1,38 +1,27 @@
 // @ts-check
 import express from 'express';
-// const fs = require('fs');
-import { appendFile } from 'fs/promises'
 import mongoose from 'mongoose';
 import registrationModel from './registration.model.js';
-// const path = require('path');
+
 
 const app = express();
-const PORT = 3001; // Make sure this port is free or change as necessary
+const {PORT = 3001, MONGO_URI = 'mongodb://localhost:27017/DB_Form'} = process.env
 
-await mongoose.connect(process.env.MONGO_URI ?? 'mongodb://localhost:27017/DB_Form');
+await mongoose.connect(MONGO_URI);
 app.use(express.json());
-app.use(express.static('public'));  // Serve static files from the current directory
+app.use(express.static('public'))
 const { log, warn } = console
-// const filePath = path.join(__dirname, 'DB_Form.csv'); // Ensures the path is always correct
+
 
 
 
 app.post('/saveData', async (req, res) => {
     log('POST request received', req.body);
-    // const { name, phone, address, participants, vacation } = req.body;
-    // for (const field of [name, phone, address, participants, vacation]) {
-    //     if (!field) {
-    //         warn({
-    //             errorType: 'missingField',
-    //             name, phone, address, participants, vacation
-    //         })
-    //         return res.status(400).send('All fields are required');
-    //     }
-    // }
 
 
-    await registrationModel.create(req.body)
+    await registrationModel.create(req.body, {})
     .then(log)
+    .then(() => res.send('Data saved successfully'))
     
     .catch(err => {})
 });
